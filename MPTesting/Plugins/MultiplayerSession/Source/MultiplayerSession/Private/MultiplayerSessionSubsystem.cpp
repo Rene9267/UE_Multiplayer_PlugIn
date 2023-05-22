bc4df537eaa6455658515a5daa0a59412bd133c5
Antonio Troiano <antonio.troiano2304@gmail.com>
2023-05-22 10:16:25 +0200

    Commit: Multiplayer Session
    - Create Multiplayer Session Subsystem
    - Stored the Session Interface in a a member variable

---
+++ MPTesting/Plugins/MultiplayerSession/Source/MultiplayerSession/Private/MultiplayerSessionSubsystem.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MultiplayerSessionSubsystem.h"
+#include "OnlineSubsystem.h"
+
+UMultiplayerSessionSubsystem::UMultiplayerSessionSubsystem()
+{
+	IOnlineSubsystem* Subsystem = IOnlineSubsystem::Get();
+	if (Subsystem)
+	{
+		SessionInterface = Subsystem->GetSessionInterface();
+	}
+}
---
+++ MPTesting/Plugins/MultiplayerSession/Source/MultiplayerSession/Public/MultiplayerSessionSubsystem.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "Interfaces/OnlineSessionInterface.h"
+
+
+#include "MultiplayerSessionSubsystem.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class MULTIPLAYERSESSION_API UMultiplayerSessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+public:
+	UMultiplayerSessionSubsystem(); //default constructor
+protected:
+
+private:
+	IOnlineSessionPtr SessionInterface;
+};
